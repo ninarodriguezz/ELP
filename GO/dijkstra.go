8aedb03991d63fbeffdcbbe00f73cbd1c119efab
ccanela <cristina.canela-anon@insa-lyon.fr>
2024-01-15 16:30:05 +0100

    ups

--- GO/dijkstra.go
+++ GO/dijkstra.go
@@ -122,7 +122,7 @@ func hello(nodeSrc *Node, nodeDst *Node) {
 	waitGroup.Done()
 }
 
-func processMessages(g *Graph, node *Node, done chan struct{}) { //je rajoute graph pour appeler la fct qui recalcule dijkstra
+func processMessages(g *Graph, node *Node) { //je rajoute graph pour appeler la fct qui recalcule dijkstra
 	for {
 		select {
 		case message := <-node.Channel:
@@ -141,8 +141,6 @@ func processMessages(g *Graph, node *Node, done chan struct{}) { //je rajoute gr
 				addLinkAndRecalculate(g, message.LinkDetails)
 			default:
 				fmt.Printf("Message de type inconnu: %s\n", message.Content)
-		case <-done:
-			return
 			}
 
 		}
@@ -335,15 +333,6 @@ func main() {
 	constructAllRoutingTables(&graph)
 
 	// Affichage table de routage pour chaque noeud
-<<<<<<< HEAD
-	// for _, start := range graph.Nodes {
-	// 	fmt.Println("\nDistances les plus courtes du noeud", start.Name)
-	// 	for dest, route := range start.RoutingTable {
-	// 		fmt.Print(start.Name, " -> ", dest, " : ", route["next_hop"].Name, "\n")
-	// 	}
-	// }
-	done := make(chan struct{})
-=======
 	for _, start := range graph.Nodes {
 		fmt.Println("\nDistances les plus courtes du noeud", start.Name)
 		for dest, route := range start.RoutingTable {
@@ -351,12 +340,11 @@ func main() {
 		}
 	}
 
->>>>>>> 7c8357a275377c1b001f9b34396cca348df83ecd
 	for nodeNumber := 0; nodeNumber < len(graph.Nodes); nodeNumber++ {
 		waitGroup.Add(1)
 
 		nodeSrc := graph.Nodes[nodeNumber]
-		go processMessages(&graph, nodeSrc, done)
+		go processMessages(&graph, nodeSrc)
 
 		nodeDst := graph.Nodes[rand.Intn(len(graph.Nodes))]
 		for nodeDst == nodeSrc {
@@ -374,12 +362,6 @@ func main() {
 	nodeA := graph.Nodes[0]
 	nodeB := nodeA.Edges[0].To
 	link_details := LinkInfo{NodeA: nodeA, NodeB: nodeB}
-<<<<<<< HEAD
-	link_failure := Message{Source: nodeA, Destination: graph.Nodes[10], Content: "link no longer available", LinkDetails: link_details}
-	sendMessage(graph.Nodes[10].Channel, link_failure)
-	processMessages(&graph, graph.Nodes[10], done)
-	close(done)
-=======
 	link_failure := Message{Source: nodeA, Destination: graph.Nodes[nodesCount-1], Content: "link no longer available", LinkDetails: link_details}
 	waitGroup.Add(2)
 	go sendMessage(graph.Nodes[nodesCount-1].Channel, link_failure)
@@ -393,7 +375,6 @@ func main() {
 			fmt.Print(start.Name, " -> ", dest, " : ", route["next_hop"].Name, "\n")
 		}
 	}
->>>>>>> 7c8357a275377c1b001f9b34396cca348df83ecd
 
 	closeChan(graph)
 
